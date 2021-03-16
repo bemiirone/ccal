c4787fd06619914b57836f44e609f034b1647f70
Bemi Irone <bemidev@nsmultimedia.co.uk>
2021-03-16 09:59:41 +0000

    added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IdeasCollectionComponent } from './ideas-collection/ideas-collection.component';
+import { HttpClientModule } from '@angular/common/http'
+import { IdeaComponent } from './idea/idea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdeasCollectionComponent,
+    IdeaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
